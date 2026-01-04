9d1d8e83292d41eb2b8e8cc87ab3abf14316ead7
alex-zhang97 <alexczhang08@gmail.com>
2026-01-03 21:59:41 -0500

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alex-zhang97.github.io',
+  base: '/Portfolio',
   vite: {
     plugins: [tailwindcss()],
   },
